60970e76de41658d7818d30232c568d110a51141
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-12-13 22:10:43 +0000

    task-4 complete

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const { getEndpoints } = require("./db/controllers/api.controller.js");
 const { getTopics } = require("./db/controllers/topics.controller.js");
-const { getArticleById } = require("./db/controllers/articles.controller.js");
+const {
+  getArticleById,
+  getAllArticles,
+} = require("./db/controllers/articles.controller.js");
 
 app.get("/api", getEndpoints);
 
@@ -10,8 +13,15 @@ app.get("/api/topics", getTopics);
 
 app.get("/api/articles/:article_id", getArticleById);
 
+app.get("/api/articles", getAllArticles);
+
 app.use("/*", (req, res) => {
   res.status(404).send({ msg: "Endpoint not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send({ msg: "Internal Server Error" });
 });
 
 module.exports = app;
