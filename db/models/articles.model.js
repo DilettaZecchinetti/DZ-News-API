1d37c3dab3fdd16d3828e3a1399e0212d0b59140
DilettaZecchinetti <diletta.zecchinetti@gmail.com>
2024-12-19 10:11:06 +0000

    fetchArticleById update

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -2,12 +2,32 @@ const db = require("../connection");
 
 const fetchArticleById = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id=$1`, [article_id])
-    .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({ status: 404, msg: "Id not found" });
+    .query(
+      `SELECT 
+        articles.author, 
+        articles.title, 
+        articles.article_id, 
+        articles.body, 
+        articles.topic, 
+        articles.created_at, 
+        articles.votes, 
+        articles.article_img_url,
+        COUNT(comments.article_id)::INT AS comment_count
+     FROM articles
+     LEFT JOIN comments 
+     ON articles.article_id = comments.article_id
+     WHERE articles.article_id = $1
+     GROUP BY articles.article_id`,
+      [article_id]
+    )
+    .then((result) => {
+      if (result.rows.length === 0) {
+        return null;
       }
-      return rows[0];
+      return result.rows[0];
+    })
+    .catch((err) => {
+      next(err);
     });
 };
 
